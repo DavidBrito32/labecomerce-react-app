c8d1ff144ca2396f9f1cbedce53ff45cb95a6fbf
DavidBrito32 <davidbrito.carneiro458@gmail.com>
2023-09-08 11:54:41 -0300

    Inclusão de mais produtos

--- src/components/componentes-main/Produtos/ListaProdutos.jsx
+++ src/components/componentes-main/Produtos/ListaProdutos.jsx
@@ -10,6 +10,18 @@ import img9 from "./IMAGENS DA FAKE API/pistola_de_gravitons.jpg";
 import img10 from "./IMAGENS DA FAKE API/sensor_lidar.jpg";
 import img11 from "./IMAGENS DA FAKE API/sonda_espacial.jpg";
 import img12 from "./IMAGENS DA FAKE API/tensor_microship.webp";
+import img13 from './IMAGENS DA FAKE API/Viagem-para-marte.png';
+import img14 from './IMAGENS DA FAKE API/Viagem-para-europa-jupter.png'
+import img15 from './IMAGENS DA FAKE API/nova-centaury.jpg';
+import img16 from './IMAGENS DA FAKE API/case.jpeg';
+import img17 from './IMAGENS DA FAKE API/traje espacial.webp';
+import img18 from './IMAGENS DA FAKE API/escudo termico.webp';
+import img19 from './IMAGENS DA FAKE API/nanotecnologia.jpg';
+import img20 from './IMAGENS DA FAKE API/mainframe de pulso.jpg';
+import img21 from './IMAGENS DA FAKE API/detector de proximidade.jpg';
+import img22 from './IMAGENS DA FAKE API/prometeus nave.webp';
+import img23 from './IMAGENS DA FAKE API/plasma.webp';
+import img24 from './IMAGENS DA FAKE API/negro.jpg';
 //import das imagens espaciais
 
 export const ListarProdutos = [
@@ -151,4 +163,136 @@ export const ListarProdutos = [
     rate: "3,5",
     promocao: false,
   },
+  {
+    id: 13,
+    image: `${img13}`,
+    nomeProduto: "Viagem para Marte",
+    precoUnitario: 130000,
+    categoria: "Starship",
+    descricao: "Uma viagem para marte, com um veiculo espacial de alta tecnologia",
+    desconto: 145000,
+    rate: "4,5",
+    promocao: true,
+  },
+  {
+    id: 14,
+    image: `${img14}`,
+    nomeProduto: "Viagem para Europa, Lua de Jupter",
+    precoUnitario: 230000,
+    categoria: "Starship",
+    descricao: "Uma viagem para Europa 13 uma lua de Jupter, com um veiculo espacial de alta tecnologia",
+    desconto: 245000,
+    rate: "4,5",
+    promocao: true,
+  },
+  {
+    id: 15,
+    image: `${img15}`,
+    nomeProduto: "Viagem para Aupha Centaury, [NOVA TERRA]",
+    precoUnitario: 2230000,
+    categoria: "Starship",
+    descricao: "Uma viagem para Europa 13 uma lua de Jupter, com um veiculo espacial de alta tecnologia",
+    desconto: 2245000,
+    rate: "5,0",
+    promocao: true,
+  },
+  {
+    id: 16,
+    image: `${img16}`,
+    nomeProduto: "Case Robo de Suporte [Interestelar]",
+    precoUnitario: 50000,
+    categoria: "NASA",
+    descricao: "Um Robo auxiliar de alta tecnologia",
+    desconto: 0,
+    rate: "5,0",
+    promocao: false,
+  },
+  {
+    id: 17,
+    image: `${img17}`,
+    nomeProduto: "Traje espacial de ultima geração",
+    precoUnitario: 17000,
+    categoria: "NASA",
+    descricao: "Traje de viagem espacial",
+    desconto: 22500,
+    rate: "3,0",
+    promocao: true,
+  },
+  {
+    id: 18,
+    image: `${img18}`,
+    nomeProduto: "Escudo termico",
+    precoUnitario: 30000,
+    categoria: "Roscosmos",
+    descricao: "Escudo termico",
+    desconto: 35250,
+    rate: "3,0",
+    promocao: true,
+  },
+  {
+    id: 19,
+    image: `${img19}`,
+    nomeProduto: "Sistema de auto-reparo",
+    precoUnitario: 17000,
+    categoria: "NASA",
+    descricao: "Sistema de auto reparo baseado em nanotecnologia",
+    desconto: 22500,
+    rate: "3,0",
+    promocao: true,
+  },
+  {
+    id: 20,
+    image: `${img20}`,
+    nomeProduto: "Mainframe de pulso",
+    precoUnitario: 1500,
+    categoria: "NASA",
+    descricao: "Mainframe de pulso que serve para monitorar as informações do traje",
+    desconto: 2000,
+    rate: "3,5",
+    promocao: true,
+  },
+  {
+    id: 21,
+    image: `${img21}`,
+    nomeProduto: "Detector de proximidade",
+    precoUnitario: 14500,
+    categoria: "Roscosmos",
+    descricao: "Detector de proximidade",
+    desconto: 16580,
+    rate: "3,5",
+    promocao: true,
+  },
+  {
+    id: 22,
+    image: `${img22}`,
+    nomeProduto: "Nave da missão prometheus",
+    precoUnitario: 1125600,
+    categoria: "Starship",
+    descricao: "Nave recuperada da missao prometheus",
+    desconto: 0,
+    rate: "3,5",
+    promocao: false,
+  },
+  {
+    id: 23,
+    image: `${img23}`,
+    nomeProduto: "Detector de plasma espacial",
+    precoUnitario: 50000,
+    categoria: "Google",
+    descricao: "Detector de plasma espacial com longo alcance",
+    desconto: 0,
+    rate: "3,5",
+    promocao: false,
+  },
+  {
+    id: 24,
+    image: `${img24}`,
+    nomeProduto: "Gerador de Buraco negro",
+    precoUnitario: 1125600,
+    categoria: "Roscosmos",
+    descricao: "Gerador de Buracos negros, de alta periculosidade",
+    desconto: 0,
+    rate: "3,5",
+    promocao: false,
+  }
 ];
